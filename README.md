710efa147ae7f59f451991e0dbb5e30d91e16797
jiqishoubi <437753880@qq.com>
2020-06-29 10:50:18 +0800

    隐藏左侧滚动条

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+记录modules更改
 npx patch-package @ant-design/pro-layout
--- config/theme/defaultTheme.js
+++ config/theme/defaultTheme.js
@@ -1,9 +1,10 @@
 const theme = {
   'font-size-base': '13px',
-  'primary-color': 'black',
+  'primary-color': '#3ca1be',
+  'layout-header-background': '#0a4a5d',
+  'menu-dark-submenu-bg': '#063c4c',
   'layout-header-height': '40px',
   'menu-collapsed-width': '44px',
-  'layout-header-background': 'black',
   //自定义
   't-siderMenu-width': '190px',
   't-istabs-tabbar-height': '35px', //多tab时，tabbar高度
